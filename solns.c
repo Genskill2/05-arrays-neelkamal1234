ee375e8eeb19743c2eb57a9eaac620d1a755b313
neelkamal1234 <84660248+neelkamal1234@users.noreply.github.com>
2021-06-16 19:25:14 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,12 +1,13 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 
-int max(int x[] , int n)
+int max(int z[] , int n)
 { int l =0;
   for( int i=0;i<n;i++){
-      if( x[i]>l )
-        l=x[i];
-    
+      if( z[i]>l ){
+        l = z[i];
+      }
+  }
     return l;
   }
      
