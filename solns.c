11e241b8f61703d79f9f6c50d82f1345cf23478c
neelkamal1234 <84660248+neelkamal1234@users.noreply.github.com>
2021-06-16 19:20:26 +0530

    Update solns.c
    added max function to the solutions  file.

--- solns.c
+++ solns.c
@@ -1,2 +1,16 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max(int x[] , int n)
+{ int l =0;
+  for( int i=0;i<n;i++){
+      if( x[i]>l )
+        l=x[i];
+    
+    return l;
+  }
+     
+    
+  
+
+
